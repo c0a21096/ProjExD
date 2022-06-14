ce0e0f374ba4515dddc3836be25a67e8bc8186f8
c0a21096 <c0a210968d@edu.teu.ac.jp>
2022-06-14 18:48:03 +0900

    修正 #5

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -5,38 +5,45 @@ MISSING_NUM = 2
 TRIALS = 5
 
 def main():
-    questioning(initgame())
+    target, display, missing = generate_list()
+    print(f"{target}\n{missing}\n{display}")
+    print(display)
+    is_qn_collect = quiestioning_num()
+    if is_qn_collect:
+        print("正解です。それでは、具体的に欠損文字を１つずつ入力してください")
+        is_qc_collect = quiestioning_chr(missing)
+        if is_qc_collect:
+            print("ゲームクリアです。")
+        else:
+            ("不正解です、またチャレンジしてください")
+    else:
+        print("不正解です。")
 
-def initgame():
+def generate_list():
     alphabet = [chr(c+65) for c in range(26)]
     target_chars = random.sample(alphabet, TARGET_NUM)
     missing_chars = random.sample(target_chars, MISSING_NUM)
     display_chars = [n for n in target_chars if n not in missing_chars]
     #print(f"{target_chars}\n{missing_chars}\n{display_chars}")
-    return display_chars, missing_chars
+    return target_chars, display_chars, missing_chars
 
-def puzzle(answer):
-    ans_l = answer
-    for i in range(TRIALS):
-        if not ans_l:
-            print("clear")
-        ans_chr = input(f"{i+1}つ目の文字を入力してください：")
-        if ans_chr in ans_l:
-            ans_l.remove(ans_chr)
-        else:
-            print("不正解です。またチャレンジしてください")
-            break
-
-def questioning(display, answer):
-    display = " ".join(display)
-    print(f"表示文字:{display}")
-
-    ans_num = input("欠損文字はいくつあるでしょうか？")
+def quiestioning_num():
+    ans_num = int(input("欠損文字はいくつあるでしょうか？:"))
     if ans_num == MISSING_NUM:
-        print("正解です。それでは具体的に欠損文字を１つずつ入力してください")
-        puzzle(answer)
-            
+        return True
     else:
-        print("不正解です。")
+        return False
 
-main()
+def quiestioning_chr(missing):
+    for i in range(TRIALS):
+        if len(missing) <= 0:
+            return True
+        ans_chr = input(f"{i+1}つ目の文字を入力してください:")
+        if ans_chr in missing:
+            missing.remove(ans_chr)
+        else:
+            break
+    return False
+
+if __name__ == "__main__":
+    main()
