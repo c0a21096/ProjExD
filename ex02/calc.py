f053d575093c39968483cc7b7d45d01abd40f83a
c0a21096 <c0a210968d@edu.teu.ac.jp>
2022-06-21 16:46:05 +0900

    四則演算、色替え機能追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,5 +1,42 @@
+"""
+追加機能:四則演算キー、初期化キー、キーボードの色変え機能
+"""
+
+from sre_parse import expand_template
 import tkinter as tk
 import tkinter.messagebox as tkm
+from turtle import color
+
+THEME_LIST = ["Blue", "Red", "Default"]
+BG = "white"
+
+def cfg_window(event): #cfg画面
+    cfg = tk.Toplevel()
+    #cfg.geometry("200x200")
+    exp = tk.Label(cfg, text="テーマを選択")
+    exp.grid(row=0, column=0)
+    for i, theme in enumerate(THEME_LIST):
+        button = tk.Button(
+            cfg, 
+            text=theme, 
+            height=1, 
+            width=10,
+            font=("Times New Roman", 20)
+            )
+        button.bind("<1>", cfg_btn_click)
+        button.grid(row=i, column=0)
+
+def cfg_btn_click(event):
+    global BG
+    btn = event.widget
+    color = btn["text"]
+    if color == "Red":
+        BG = "red"
+    elif color == "Blue":
+        BG = "blue"
+    else:
+        BG = "white"
+    init_keys()
 
 def button_click(event):
     btn = event.widget
@@ -9,29 +46,45 @@ def button_click(event):
         res = eval(eq)
         entry.delete(0, tk.END)
         entry.insert(tk.END, res)
+    elif num == "C":
+        entry.delete(0, tk.END)
     else:
         entry.insert(tk.END, num)
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    #root.geometry("300x500")
-    root.title("計算機")
-
-    r, c = 0, 0
-    for i, num in enumerate([9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "="]):
+def init_keys():
+    r, c = 0, 0 #行r、列c
+    for i, num in enumerate([9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "-", "*", "/", "=", "C"]):
         button = tk.Button(
             root, 
             text=f"{num}", 
             height=2, 
             width=4,
-            font=("Times New Roman", 30)
+            font=("Times New Roman", 30),
+            bg = BG
             )
         button.bind("<1>", button_click)
         button.grid(row=r+1, column=c)
         c+=1
-        if (i+1)%3==0:
+        if (i+1)%4==0:
             r+=1
             c=0
+        cfgbutton = tk.Button(
+            root, 
+            text="Config", 
+            height=1, 
+            width=50,
+            font=("Times New Roman", 10)
+            )
+    cfgbutton.bind("<1>", cfg_window)
+    cfgbutton.grid(row=r+1, column=c, columnspan=4)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    #root.geometry("300x500")
+    root.title("計算機")
+    sub_win = None
+
+    init_keys()
 
     entry = tk.Entry(
             root, 
@@ -39,6 +92,6 @@ if __name__ == "__main__":
             width=10, 
             font=("Times New Roman", 40)
             )
-    entry.grid(columnspan=3, column=0, row=0)
+    entry.grid(columnspan=4, column=0, row=0)
 
     root.mainloop()
