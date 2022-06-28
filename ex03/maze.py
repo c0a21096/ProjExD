ea444790c2ca476a4d37ee794192bed8995e1acb
c0a21096 <c0a210968d@edu.teu.ac.jp>
2022-06-28 14:32:59 +0900

    練習問題5

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+def key_down(event):
+    global key
+    key = event.keysym
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -16,5 +20,9 @@ if __name__ == "__main__":
     koukaton = tk.PhotoImage(file="fig/0.png")
     cx, cy = 300, 400
     canvas.create_image(cx, cy, image=koukaton, tag="koukaton")
+    
+    key = ""
+    root.bind("<KeyPress>", key_down)
+
     root.mainloop()
     
