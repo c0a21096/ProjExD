211e443b6806e4df2e68a2868a0c3578ea776469
c0a21096 <c0a210968d@edu.teu.ac.jp>
2022-07-05 14:27:23 +0900

    練習問題3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,8 +2,29 @@ import pygame as pg
 import sys
 
 def main():
+    clock = pg.time.Clock()
     pg.display.set_caption("逃げろ！こうかとん")
-    screen = pg.display.set_mode((1600, 900)) #surface
+    screen_sfc = pg.display.set_mode((1600, 900)) #surface
+    screen_rect = screen_sfc.get_rect()
+
+    bgimg_sfc = pg.image.load("fig/pg_bg.jpg")
+    bgimg_rect = bgimg_sfc.get_rect()
+    screen_sfc.blit(bgimg_sfc, bgimg_rect)
+
+    koukatonimg_sfc = pg.image.load("fig/6.png")
+    koukatonimg_sfc = pg.transform.rotozoom(koukatonimg_sfc, 0, 2.0) 
+    koukatonimg_rect = koukatonimg_sfc.get_rect()
+    koukatonimg_rect.center = 900, 400
+
+    while True:
+        screen_sfc.blit(bgimg_sfc, bgimg_rect)
+        screen_sfc.blit(koukatonimg_sfc, koukatonimg_rect)
+        
+        for event in pg.event.get():
+            if event.type == pg.QUIT: 
+                return
+        pg.display.update()
+        clock.tick(1000)
 
 if __name__ == "__main__":
     pg.init()
